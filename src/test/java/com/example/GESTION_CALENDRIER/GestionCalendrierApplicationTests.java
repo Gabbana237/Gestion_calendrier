164825feac5b321da0d820e00d93882e682f153b
hamed <mefirehamed936@gmail.com>
2024-04-07 21:23:44 +0100

    initial commit

---
+++ src/test/java/com/example/GESTION_CALENDRIER/GestionCalendrierApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.GESTION_CALENDRIER;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestionCalendrierApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
